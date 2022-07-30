17639445b022af86844c36b8be803b72cadf3f47
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-30 13:02:57 +0530

    Footer Icons in Post in Done

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
-import { DotsHorizontalIcon } from "@heroicons/react/outline";
+import {
+  DotsHorizontalIcon,
+  HeartIcon,
+  ChatIcon,
+  PaperAirplaneIcon,
+  BookmarkIcon,
+} from "@heroicons/react/outline";
 const Post = ({ id, username, userImg, img, caption }) => {
   return (
     <div className="bg-white my-7 border rounded-sm shadow-sm">
@@ -14,6 +20,14 @@ const Post = ({ id, username, userImg, img, caption }) => {
         <DotsHorizontalIcon className="h-5 cursor-pointer" />
       </div>
       <img src={img} alt="post-image" className="object-cover w-full" />
+      <div className="flex items-center justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
     </div>
   );
 };
