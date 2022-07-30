ca3bd3321148c3b31e5b5b0a20d58369445ca04d
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-30 17:49:24 +0530

    Uploading the Image with Post information to Firestore and Cloud Storge is Suffessfully Done ✅

--- components/Modal.js
+++ components/Modal.js
@@ -3,13 +3,53 @@ import { useAuth } from "../context/useAuth";
 import { Dialog, Transition } from "@headlessui/react";
 import { CameraIcon } from "@heroicons/react/outline";
 import { useRef } from "react";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 const Modal = () => {
-  const { modal } = useAuth();
+  const { modal, user } = useAuth();
   const [modalStatus, setModalStatus] = modal;
   const filePickerRef = useRef(null);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
   const captionRef = useRef(null);
+
+  const uploadPost = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db, "post"), {
+      username: user.displayName,
+      caption: captionRef.current.value,
+      profileImg: user.photoURL,
+      timestamp: serverTimestamp(),
+    });
+
+    console.log("🚀 New Doc Added with ID: ", docRef.id);
+
+    const imageRef = ref(storage, `post/${docRef.id}/image`);
+
+    await uploadString(imageRef, selectedFile, "data_url").then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "post", docRef.id), {
+          image: downloadURL,
+        });
+        console.log("🚀 New Doc Added with Post Image ID: ", docRef.id);
+      }
+    );
+
+    setModalStatus(false);
+    setLoading(false);
+    setSelectedFile(null);
+  };
 
   const addImagetoPost = (e) => {
     const reader = new FileReader();
@@ -98,8 +138,12 @@ const Modal = () => {
                   />
                 </div>
                 <div className="mt-5 sm:mt-6">
-                  <button className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-0 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300">
-                    Upload Post
+                  <button
+                    onClick={uploadPost}
+                    disabled={!selectedFile}
+                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-0 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300"
+                  >
+                    {loading ? "Uploading Post" : "Upload Post"}
                   </button>
                 </div>
               </div>
