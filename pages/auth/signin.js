63cfb67e500493e11e22fc8e2500add862ff0a49
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-30 14:26:12 +0530

    Next Provider Changed from server side to UseEffect Async function

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProviders, signIn } from "next-auth/react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const [providers, setProviders] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const providers = await getProviders();
+      setProviders(providers);
+    })();
+  }, []);
+
+  console.log(providers);
+  return (
+    <>
+      {providers &&
+        Object?.values(providers)?.map((provider) => (
+          <div key={provider.name}>
+            <button onClick={() => signIn(provider.id)}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+    </>
+  );
 };
 
 export default SignIn;
